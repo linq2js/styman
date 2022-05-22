fc5e6857c244034ca9ed7857aa7033897bf84ecb
hung.nguyen <linqtojs@gmail.com>
2022-05-22 16:43:25 +0700

    fix: font-weight

--- packages/styman/lib/styler/fontModule.ts
+++ packages/styman/lib/styler/fontModule.ts
@@ -52,13 +52,13 @@ export const fontModule = <C extends ColorScheme, M extends Modifiers>({
       ...withValues(
         {
           thin: 100,
-          extraLight: 200,
+          extralight: 200,
           light: 300,
           normal: 400,
           medium: 500,
           semiBold: 600,
           bold: 700,
-          extraBold: 800,
+          extrabold: 800,
           black: 900,
         },
         (x) => ({ fontWeight: x })
