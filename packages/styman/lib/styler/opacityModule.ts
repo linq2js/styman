9925e059f6df3f2e2c3fa5991b73bc9cf822fb13
hung.nguyen <linqtojs@gmail.com>
2022-05-25 19:50:22 +0700

    fix: opacity

--- packages/styman/lib/styler/opacityModule.ts
+++ packages/styman/lib/styler/opacityModule.ts
@@ -5,6 +5,8 @@ export const opacityModule = <C extends ColorScheme, M extends Modifiers>({
   withModifiers,
 }: BuildContext<C, M>) => {
   return {
-    ...withModifiers("opacity", { $number: (x: number) => ({ opacity: x }) }),
+    ...withModifiers("opacity", {
+      $number: (x: number) => ({ opacity: x / 100 }),
+    }),
   };
 };
