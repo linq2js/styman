60a2aed206acdb6d73e5671edf09dc6bd4deddfa
hung.nguyen <linqtojs@gmail.com>
2022-05-25 14:03:27 +0700

    feat: styled

---
+++ packages/styman/lib/styled/index.ts
@@ -0,0 +1,44 @@
+import { RuleSet, Sheet, css } from "../main";
+import { ReactHTML, HTMLAttributes, FC, createElement } from "react";
+
+export type Styled<R extends RuleSet> = {
+  <T extends keyof ReactHTML>(element: T): (
+    template: TemplateStringsArray,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ...args: any[]
+  ) => FC<HTMLAttributes<T>>;
+  <T extends keyof ReactHTML>(element: T, ...args: Parameters<typeof css>): FC<
+    HTMLAttributes<T>
+  >;
+  (...args: Parameters<Sheet<R>>): { className: string };
+};
+
+export const createStyled = <R extends RuleSet>(sheet: Sheet<R>) => {
+  return ((...args: any[]) => {
+    if (typeof args[0] === "string") {
+      if (args.length === 1) {
+        const tag = args[0];
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return (template: TemplateStringsArray, ...args: any[]) => {
+          return function StyledComponent<TProps>(props: TProps): FC<TProps> {
+            return createElement(tag, {
+              ...props,
+              className: css(
+                template,
+                ...args.map((a) => (typeof a === "function" ? a(props) : a))
+              ),
+            }) as unknown as FC<TProps>;
+          };
+        };
+      }
+      const [tag, ...styles] = args;
+      return function StyledComponent<TProps>(props: TProps): FC<TProps> {
+        return createElement(tag, {
+          ...props,
+          className: sheet(...(styles as Parameters<Sheet<R>>)),
+        }) as unknown as FC<TProps>;
+      };
+    }
+    return { className: sheet(...(args as Parameters<Sheet<R>>)) };
+  }) as Styled<R>;
+};
